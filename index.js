0c31a275059e5bc23fd526dea007f93e44c0a513
enorvang <espen.norvang@gmail.com>
2020-11-07 16:43:57 +0100

    moved js-file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import nedbetalingsKalkulator from "./nedbetalingsKalkulator";
+import nedbetalingsKalkulator from "./js/nedbetalingsKalkulator";
 
 const app = express();
 
